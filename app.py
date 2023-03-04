c344038566e9418c981e234b4cadb799645d7606
Nvect143 <101389139+Nvect143@users.noreply.github.com>
2023-03-04 19:26:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import plost
+
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style', unsafe_allow_html=True)
+
+st.sidebar.header('Streamlit Dashboard')
+
+st.sidebar.subheader('Heat map parameter')
+time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max'))
+
+st.sidebar.subheader('Donut Chart parameter')
+donut_theta = st.sidebar.selectbox('Select Data', ('q2', 'q3'))
+
+st.sidebar.subheader('Line Chart Parameters')
+plot_data = st.sidebar.multiselect('Select Data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
+plot_height = st.sidebar.slider('Specify Plot Height', 200, 500, 250)
+
+st.markdown('Metrics')
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+
+settle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
+stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
+
+c1, c2 = st.columns((7,3))
+
+with c1:
+    st.markdown('### Heatmap')
+    plost.time_hist(
+    data=settle_weather,
+    date='date',
+    x_unit='week',
+    y_unit='day',
+    color=time_hist_color,
+    aggregate='median',
+    legend=None,
+    height=345,
+    use_container_width=True)
+with c2:
+    st.markdown('### Donut chart')
+    plost.donut_chart(
+        data=stocks,
+        theta=donut_theta,
+        color='company',
+        legend='bottom',
+        use_container_width=True)
+
+# Row C
+st.markdown('### Line chart')
+st.line_chart(settle_weather, x = 'date', y = plot_data, height = plot_height)
